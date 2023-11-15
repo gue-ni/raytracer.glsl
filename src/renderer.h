64dcb245868cf27d207ae1e6063c93f0073dfc1b
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-11-15 13:57:25 +0100

    update

--- src/renderer.h
+++ src/renderer.h
@@ -1,4 +1,7 @@
 #include "window.h"
+#include "gfx/gfx.h"
+
+#include <memory>
 
 class Renderer : public Window {
 public:
@@ -6,4 +9,11 @@ public:
   void render(float dt) override;
 
 private:
+  std::unique_ptr<gfx::gl::Shader> m_screen_shader = nullptr; 
+  std::unique_ptr<gfx::gl::Shader> m_render_shader = nullptr;
+
+  std::unique_ptr<gfx::gl::VertexBufferObject> m_screen_quad_vao = nullptr;
+  std::unique_ptr<gfx::gl::VertexBuffer> m_screen_quad_vbo = nullptr;
+  
+  std::unique_ptr<gfx::gl::Texture> m_texture = nullptr;
 };
