553652990fd95fd8ed128cc88de39ae01e2e220b
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-11-15 14:15:50 +0100

    update

--- src/renderer.h
+++ src/renderer.h
@@ -17,7 +17,7 @@ private:
   std::unique_ptr<gfx::gl::Shader> m_screen_shader = nullptr; 
   std::unique_ptr<gfx::gl::Shader> m_render_shader = nullptr;
 
-  std::unique_ptr<gfx::gl::VertexBufferObject> m_screen_quad_vao = nullptr;
+  std::unique_ptr<gfx::gl::VertexArrayObject> m_screen_quad_vao = nullptr;
   std::unique_ptr<gfx::gl::VertexBuffer> m_screen_quad_vbo = nullptr;
   
   std::unique_ptr<gfx::gl::Texture> m_texture = nullptr;
