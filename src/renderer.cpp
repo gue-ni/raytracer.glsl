e1b8253a0b88f779ed5e5ba8588fe09980d1fd64
Jakob <45669207+gue-ni@users.noreply.github.com>
2023-11-16 14:44:16 +0100

    update

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -35,6 +35,13 @@ Renderer::Renderer(int width, int height)
   m_screen_quad_vao->unbind();
 
   // setup texture
+
+  m_texture->bind();
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, m_width, m_height, 0, GL_RGBA, GL_FLOAT, NULL);
 }
 
 void Renderer::render(float dt)
@@ -43,10 +50,15 @@ void Renderer::render(float dt)
   glClear(GL_COLOR_BUFFER_BIT);
 
   // dispatch compute shaders
-  // TODO
+  glBindImageTexture(0, m_texture->id(), 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
+  glDispatchCompute(m_width, m_height, 1);
+  glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+  m_texture->bind(0);
 
   // draw scren quad
   m_screen_shader->bind();
+  m_screen_shader->set_uniform("u_Texture", 0);
   m_screen_quad_vao->bind();
   glDrawArrays(GL_TRIANGLES, 0, 3 * 2);
 }
