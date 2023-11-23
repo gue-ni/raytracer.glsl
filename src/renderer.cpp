d9640764f0690f74e23fbd33c2d55b86207f2251
Jakob <jakob.g.maier@gmail.com>
2023-11-23 18:43:55 +0100

    update

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -11,7 +11,7 @@ using namespace gfx::gl;
 Renderer::Renderer(int width, int height) 
   : Window(width, height)
   , m_screen_shader(std::make_unique<ShaderProgram>(ShaderProgram::string_from_file("shaders/screen.vert"), ShaderProgram::string_from_file("shaders/screen.frag")))
-  , m_render_shader(std::make_unique<ShaderProgram>(ShaderProgram::string_from_file("shaders/pathtracer.glsl")))
+  , m_render_shader(std::make_unique<ShaderProgram>(ShaderProgram::string_from_file("shaders/raytracer.glsl")))
   , m_texture(std::make_unique<Texture>())
   , m_screen_quad_vao(std::make_unique<VertexArrayObject>())
   , m_screen_quad_vbo(std::make_unique<VertexBuffer>())
